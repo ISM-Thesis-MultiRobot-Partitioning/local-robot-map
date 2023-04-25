b19ba8c16f66447c056b4c66ba67606c74ae5e11
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-04-25 12:09:14 +0200

    start polygon to cellmap conversion

--- src/polygon_map.rs
+++ src/polygon_map.rs
@@ -1,5 +1,10 @@
+use geo::BoundingRect;
+use geo_rasterize::BinaryBuilder;
+use matrix::prelude::Conventional;
+
 use crate::cell_map::CellMap;
 use crate::coords::Coords;
+use crate::{CellMapSize, MapState};
 
 /// Describe a map using a polygon.
 ///
@@ -17,11 +22,18 @@ use crate::coords::Coords;
 /// ```
 pub struct PolygonMap {
     vertices: Vec<Coords>,
+    polygon: geo::Polygon,
 }
 
 impl PolygonMap {
     pub fn new(vertices: Vec<Coords>) -> PolygonMap {
-        PolygonMap { vertices }
+        let polygon = geo::Polygon::new(
+            geo::LineString::from(
+                vertices.iter().map(|e| (e.x, e.y)).collect::<Vec<_>>(),
+            ),
+            vec![],
+        );
+        PolygonMap { vertices, polygon }
     }
 
     /// Convert this map to a [`CellMap`].
@@ -29,11 +41,92 @@ impl PolygonMap {
     /// The [`CellMap`] is more straightforward to work with, hence this
     /// function allows to easily make the conversion. The [`PolygonMap`] is
     /// mostly interesting for specifying a map region.
-    pub fn to_cell_map(self) -> CellMap {
-        todo!("Implement conversion");
+    ///
+    /// The `resolution` parameter specify the pixels per meter.
+    pub fn to_cell_map(self, resolution: f64) -> CellMap {
+        CellMap {
+            cells: self.rasterize_polygon(resolution),
+            resolution,
+            offset: Coords {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+        }
+    }
+
+    /// .
+    ///
+    /// # Panics
+    ///
+    /// The [`geo`] crate allows to obtain the polygon's *bounding box* as well
+    /// *rasterizing* it. Both of these procedures can cause panics, which
+    /// is not expected to happen. They boil down to **invalid polygon shapes**
+    /// and **NaN or infinite values** in the vertex coordinates.
+    ///
+    /// - Extract the Bounding Box: will panic if no bounding box can be made. A
+    ///   properly formed polygon should always have a properly defined bounding
+    ///   box. It should be checked elsewhere that the polygons have a valid
+    ///   shape.
+    /// - The *BinaryBuilder* used to rasterize the polygon can panic as well if
+    ///   there are NaN of infinite values.
+    /// - The rasterization itself can panic as well if there are NaN of
+    ///   infinite values.
+    fn rasterize_polygon(&self, resolution: f64) -> Conventional<MapState> {
+        let bbox = match self.polygon.bounding_rect() {
+            Some(b) => b,
+            None => panic!("No bounding box for polygon"),
+        };
+        let width = (bbox.width() * resolution) as usize;
+        let height = (bbox.height() * resolution) as usize;
+
+        let mut r = BinaryBuilder::new()
+            .width(width)
+            .height(height)
+            .build()
+            .expect("There should be no NaN or infinite values among the polygon vertices");
+        r.rasterize(&self.polygon)
+            .expect("There should be no NaN of infinite values");
+
+        let pixels = r.finish();
+        // - to_vec
+        // - filter on bool values
+        // - create sparse matrix
+        // - convert to cell map
+
+        let size = CellMapSize {
+            p1: &Coords {
+                x: bbox.min().x,
+                y: bbox.min().y,
+                z: 0.0,
+            },
+            p2: &Coords {
+                x: bbox.max().x,
+                y: bbox.max().y,
+                z: 0.0,
+            },
+            resolution,
+        };
+        let cells = Conventional::new(size);
     }
 
     pub fn get_vertices(&self) -> &Vec<Coords> {
         &self.vertices
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn polygon_map_to_cell_map() {
+        let p1 = Coords::new(0.0, 0.0, 0.0);
+        let p2 = Coords::new(1.0, 1.0, 0.0);
+        let p3 = Coords::new(2.0, 0.0, 0.0);
+        let polygon = PolygonMap::new(vec![p1, p2, p3]);
+        let resolution = 1.0;
+        let cellmap = polygon.to_cell_map(resolution);
+        println!("{:?}", cellmap.cells());
     }
 }
